82e851019e0287ff9e3bd44225f71049344b56d0
Chloe <art.cchu@gmail.com>
2022-10-12 20:09:55 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n;
+        if (args.length > 0) {
+            try {
+                n = Integer.parseInt(args[0]);
+                System.out.println(fib(n));
+            } catch (NumberFormatException e) {
+                System.err.println("Argument " + args[0] + " must be an integer!");
+                System.exit(1);
+            }
+        }
+    }
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
